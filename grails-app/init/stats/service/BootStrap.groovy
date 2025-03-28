c05a75aa03d45a0433c6cf4e3ee6defc0611d373
vheekey <vicformidable@gmail.com>
2025-03-28 20:32:03 +0100

    Adds log file

--- grails-app/init/stats/service/BootStrap.groovy
+++ grails-app/init/stats/service/BootStrap.groovy
@@ -3,6 +3,8 @@ package stats.service
 class BootStrap {
 
     def init = { servletContext ->
+        // Create logs directory if it doesn't exist
+        new File('logs').mkdirs()
     }
     def destroy = {
     }
