651f9e7744f4ecc01e13923d23da0c59a62017ab
vheekey <vicformidable@gmail.com>
2025-04-08 07:24:56 +0200

    Implements kafka consumer listener

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,11 @@
+package spring
+
+import stats.repositories.InfluxDBRepository
+import stats.service.InfluxDBConnectionService
+
 // Place your Spring DSL code here
 beans = {
+    influxDBConnectionService(InfluxDBConnectionService)
+    influxDBRepository(InfluxDBRepository)
+    kafkaConfig(KafkaConfig)
 }
